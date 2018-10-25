1e3bd534c38aa6576979374ad6f2aef0c695d9ca
Diego Andres Vinasco Trujillo <diegovt1@LABLINUX.PUJ.EDU.CO>
2018-10-24 19:15:02 -0500

    Tarea_DecoderVHDL

--- Tarea1.vhd
+++ Tarea1.vhd
@@ -4,16 +4,15 @@ use ieee.std_logic_unsigned.all;
 
 entity Tarea1 is
 port(
-	B1:in std_logic;
-	B0:in std_logic;
-	F:out std_logic_vector(3 downto 0)
+	B0,B1:in std_logic;
+	F0,F1,F2,F3:out std_logic
 );
 end entity;
 
 architecture Tarea1_arc of Tarea1 is
 begin
-	F(0) <= (NOT B1) AND (NOT B0);
-	F(1) <= (NOT B1) AND B0;
-	F(2) <= B1 AND (NOT B0);
-	F(3) <= B1 AND B0;
+	F0 <= (NOT B1) AND (NOT B0);
+	F1 <= (NOT B1) AND B0;
+	F2 <= B1 AND (NOT B0);
+	F3 <= B1 AND B0;
 end architecture;
