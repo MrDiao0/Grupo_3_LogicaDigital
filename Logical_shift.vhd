7a8677af1fc27965adcb89b4a1ebb3702c816a99
Diego Vinasco <MrDiao0>
2018-10-26 14:18:29 -0500

    Corrimiento_Binario_VHDL

--- Logical_shift.vhd
+++ Logical_shift.vhd
@@ -5,7 +5,8 @@ entity Logical_shift is
 port(
 	y:in std_logic_vector(3 downto 0);
 	shift:in std_logic;
-	w:out std_logic_vector(3 downto 0)
+	w:out std_logic_vector(3 downto 0);
+	k:out std_logic
 );
 end entity;
 architecture Logical_shift_arc of Logical_shift 	is
@@ -14,8 +15,10 @@ begin
 	begin
 		if(shift='1')then
 			w <= ('0',y(3),y(2),y(1));
+			k <= y(0);
 		else
 			w <= y;
+			k <= '0';
 		end if;			
 	end process;
 end architecture;
